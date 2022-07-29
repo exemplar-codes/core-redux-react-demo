11eab170b78d899440f2777120f89276be9f7d69
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-07-29 13:35:56 +0530

    Add third demo

---
+++ src/store/storeUITree.js
@@ -0,0 +1,23 @@
+import { createStore } from "redux";
+
+const reducer = (state = { count: 0 }, action) => {
+  console.log("Reducer called");
+  let newState;
+  switch (action.type) {
+    case "+":
+      newState = { ...state, count: state.count + 1 };
+      break;
+
+    case "-":
+      newState = { ...state, count: state.count - 1 };
+      break;
+
+    default:
+      newState = state;
+  }
+  return newState;
+};
+
+const storeUITree = createStore(reducer);
+
+export default storeUITree;
